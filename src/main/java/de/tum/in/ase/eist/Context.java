914dba2c9c0b27243ede9ad94c55807a8459ed8c
ebrahimudaipurwala <ebrahim.udaipurwala@gmail.com>
2025-07-13 17:54:49 +0200

    2.1

---
+++ src/main/java/de/tum/in/ase/eist/Context.java
@@ -0,0 +1,43 @@
+package de.tum.in.ase.eist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Context {
+    private List<Chapter> book;
+    private SearchStrategy searchAlgorithm;
+
+    public Context(SearchStrategy searchAlgorithm) {
+        this.book = new ArrayList<>();
+        this.searchAlgorithm = searchAlgorithm;
+    }
+
+    public List<Chapter> getBook() {
+        return book;
+    }
+
+    public void setBook(List<Chapter> book) {
+        this.book = book;
+    }
+
+    public SearchStrategy getSearchAlgorithm() {
+        return searchAlgorithm;
+    }
+
+    public void setSearchAlgorithm(SearchStrategy searchAlgorithm) {
+        this.searchAlgorithm = searchAlgorithm;
+    }
+
+    public boolean isChaptersSortedByName() {
+        for (int i = 0; i < this.book.size(); i++) {
+            int comparison = book.get(i).getName().compareTo(book.get(i + 1).getName());
+            if (comparison < 0) {
+                return false;
+            }
+            else if (comparison > 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
